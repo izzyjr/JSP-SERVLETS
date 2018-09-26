cfa7d581aab22226e75a8f1909df270fdd36f2d7
Israel Mesa <mesa.israel50@gmail.com>
2018-09-26 13:57:29 -0400

    doPost, doGet - retrieve parameters from Form

--- src/org/koushik/javabrains/XmlServlet.java
+++ src/org/koushik/javabrains/XmlServlet.java
@@ -1,6 +1,7 @@
 package org.koushik.javabrains;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 public class XmlServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("XML Servlet called");
+		System.out.println("XML Servlet called - doGet");
+		response.setContentType("text/html");
+		String userName = request.getParameter("userName");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello from GET Method: " + userName + "!</h1>");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("XML Servlet called - doPost");
+		response.setContentType("text/html");
+		String userName = request.getParameter("userName");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello from POST Method: " + userName + "!</h1>");
 	}
 	
 }
