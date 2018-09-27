56b30de39184f435fe050eeaf817ca0ee6cbb18b
Israel Mesa <mesa.israel50@gmail.com>
2018-09-26 21:03:18 -0400

    Context object

--- src/org/koushik/javabrains/SimpleServlet.java
+++ src/org/koushik/javabrains/SimpleServlet.java
@@ -3,6 +3,7 @@ package org.koushik.javabrains;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,9 +26,11 @@ public class SimpleServlet extends HttpServlet {
 		response.setContentType("text/html");
 		String userName = request.getParameter("userName");
 		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
 
 		if (userName != "" && userName != null) {
 			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
 		}
 		
 		PrintWriter writer = response.getWriter();
@@ -36,6 +39,8 @@ public class SimpleServlet extends HttpServlet {
 		writer.println("<h3>Hello " + userName + "!</h3>");
 		writer.println("<h3>Request Parameter has Username: " + userName + "</h3>");
 		writer.println("<h3>Session Parameter has Username: " + (String) session.getAttribute("savedUserName") + "</h3>");
+		writer.println("<h3>Context Parameter has Username: " + (String) context.getAttribute("savedUserName") + "</h3>");
+
 	}
 
 }
